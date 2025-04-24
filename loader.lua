d6538aa038e3b6bf3fd184f9fc60e1f515875c35
yj5v <51840817+yj5v@users.noreply.github.com>
2025-04-24 10:26:19 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,7 +8,7 @@ local Faker = shared.Faker
 Faker.Modules = {}
 
 function Faker.__loadModule(moduleName: string)
-	Faker.Modules[moduleName] = loadstring(game:HttpGet(`https://raw.githubusercontent.com/yj5v/fake-rivals-first-person-models/main/modules/{moduleName}.lua`))()
+	Faker.Modules[moduleName] = loadstring(game:HttpGet(`https://raw.githubusercontent.com/yj5v/fake-rivals-first-person-models/main/modules/{moduleName}.lua`, true))()
 end
 
 Faker.__loadModule("TweenInfos")
