cf2110ebfb61e12c166894be3582fbe83c93e11c
yj5v <51840817+yj5v@users.noreply.github.com>
2025-04-24 10:07:26 -0700

    Create TweenInfos.lua

---
+++ modules/TweenInfos.lua
@@ -0,0 +1,26 @@
+return {
+	Linear = {
+		EasingStyle = Enum.EasingStyle.Linear,
+		TimeMultiplier = 1,
+	},
+	
+	Cubic = {
+		EasingStyle = Enum.EasingStyle.Cubic,
+		TimeMultiplier = 1,
+	},
+	
+	Elastic = {
+		EasingStyle = Enum.EasingStyle.Elastic,
+		TimeMultiplier = 1,
+	},
+	
+	Bounce = {
+		EasingStyle = Enum.EasingStyle.Bounce,
+		TimeMultiplier = 1,
+	},
+	
+	Constant = {
+		EasingStyle = Enum.EasingStyle.Linear,
+		TimeMultiplier = 0,
+	}
+}
