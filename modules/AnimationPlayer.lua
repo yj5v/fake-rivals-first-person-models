220e0009bc116f8619d452cb1882bb2d2ba9a57d
yj5v <51840817+yj5v@users.noreply.github.com>
2025-04-28 06:17:04 -0700

    Update AnimationPlayer.lua

--- modules/AnimationPlayer.lua
+++ modules/AnimationPlayer.lua
@@ -181,8 +181,9 @@ function AnimationPlayer:playAnimation(name, weight, priority, speed, looped, st
 	for i = #self.activeAnimations, 1, -1 do
 		local a = self.activeAnimations[i]
 		if a.name == name then
-			anim.weight = a.weight
-			table.remove(self.activeAnimations, i)
+			a.targetWeight = weight
+			a.fadeSpeed = fadeSpeed
+			return
 		end
 	end
 
